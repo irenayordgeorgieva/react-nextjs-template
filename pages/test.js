120db50966611ae2693b4db9b13e9b34ad57c696
Irena Georgieva <irenayordgeorgieva@gmail.com>
2020-10-05 15:03:02 +0300

    Resolves #1

---
+++ pages/test.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Test = () => (
+  <div>
+    <h1>Test page</h1>
+    <Link href="/">
+      <a>Go to home page</a>
+    </Link>
+  </div>
+)
+
+export default Test
